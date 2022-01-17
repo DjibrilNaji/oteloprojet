cdbb500142d0848c28b06623000aaa486892abda
Djibril <najidjibril@gmail.com>
2022-01-17 11:34:31 +0100

    fix otelo_users

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -65,7 +65,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirmer le mot de passe) }}</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
@@ -75,7 +75,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Confimrer le mot de passe') }}
+                                    {{ __('Inscription') }}
                                 </button>
                             </div>
                         </div>
