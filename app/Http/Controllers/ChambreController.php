6efe924737008dc78074023d230bf470fba452b1
DjibrilNaji <najidjibril@gmail.com>
2022-04-08 15:49:25 +0200

    requete reservation

--- app/Http/Controllers/ChambreController.php
+++ app/Http/Controllers/ChambreController.php
@@ -67,10 +67,13 @@ class ChambreController extends Controller
         $dateF = $request->input('datef');
 
         $categories = Categorie::all();
+
         $disponibilite = DB::select('select id, nbCouchage, porte, etage, libelle, baignoire, prixBase, description from otelo_chambre
-        inner join otelo_categorie on otelo_chambre.categorie_id=otelo_categorie.categorie_id where otelo_chambre.categorie_id=? and otelo_chambre.id not in
-        (select otelo_reservation.idChambre from otelo_reservation where (?<=dateF and ?>=dateD) or (?>=dateD and ?<=dateF)) order by id ;',
-            [$categorie_id, $dateD, $dateD, $dateF, $dateF]);
+        inner join otelo_categorie on otelo_chambre.categorie_id=otelo_categorie.categorie_id
+        where otelo_chambre.categorie_id=? and otelo_chambre.id not in (select otelo_reservation.idChambre
+        from otelo_reservation where (?>=dateD and ?>=dateD and ?<=dateF and ?<=dateF)
+        or (?>=dateD and ?<=dateF) or (?>=dateD and ?<=dateF) or (?<=dateD and ?>=dateF)) order by id ;',
+            [$categorie_id, $dateD, $dateF, $dateD, $dateF, $dateD, $dateD, $dateF, $dateF, $dateD, $dateF]);
 
         return view('dispo', ['disponibilite' => $disponibilite, 'categories' => $categories]);
     }
